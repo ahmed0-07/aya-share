0c7f3259796027a4aef998ac9e4e58dfed0d6195
Ahmed Mohamed <ahmedmohamed2000771@gmail.com>
2025-11-19 23:14:07 +0200

    added some database configs

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log(3)
+import express from 'express';
+import config from './config/env.js';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.listen(config.PORT, () => {
+    console.log('server in development');
+})
--- src/config/env.ts
+++ src/config/env.ts
@@ -3,4 +3,7 @@ dotenv.config();
 
 export default {
     DATABASE_URL: process.env.DATABASE_URL,
+    PORT: process.env.PORT,
+    MODE: process.env.MODE,
+    DATABASE_DEV:process.env.DATABASE_DEV
 };

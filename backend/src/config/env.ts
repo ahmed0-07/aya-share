bcf95d0966869cf7df9c0ba34ac40fcbe14f5b80
Ahmed Mohamed <ahmedmohamed2000771@gmail.com>
2025-11-21 14:55:42 +0200

    implemented passport with google strategy

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -5,5 +5,8 @@ export default {
     DATABASE_URL: process.env.DATABASE_URL,
     PORT: process.env.PORT,
     MODE: process.env.MODE,
-    DATABASE_DEV:process.env.DATABASE_DEV
+    DATABASE_DEV: process.env.DATABASE_DEV,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET ,
+    GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL
 };
--- backend/src/config/passport.ts
+++ backend/src/config/passport.ts
@@ -1,1 +1,42 @@
 import passport from "passport";
+import { Strategy as GoogleStrategy, type Profile, type VerifyCallback } from "passport-google-oauth20";
+import config  from "./env.js";
+import prisma from "./prisma.js";
+
+
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID!,
+    clientSecret: config.GOOGLE_CLIENT_SECRET!,
+    callbackURL: config.GOOGLE_CALLBACK_URL
+},
+    async (accessToken: string, refreshToken: string, profile: Profile, done: VerifyCallback) => {
+        try{
+            const email = profile.emails?.[0]?.value;
+
+            if(!email){
+                return done(new Error("No email found in Google profile"), undefined);
+            }
+
+            let user = await prisma.user.findUnique({
+                where: {
+                    googleId: profile.id
+                }
+            });
+
+            if(!user){
+                user = await prisma.user.create({
+                    data: {
+                        name: profile.displayName,
+                        email: email,
+                        googleId: profile.id,
+                    }
+                });
+            }
+
+            return done(null, user);
+        }
+        catch(error){
+            return done(error, undefined);
+        }
+    }
+))

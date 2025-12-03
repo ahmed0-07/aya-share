8de48d80fbfecfa5a513a328cccf05c3deae3a4d
Ahmed Mohamed <ahmedmohamed2000771@gmail.com>
2025-12-03 13:26:25 +0200

    handled video errors

--- backend/src/modules/Video/Video.controller.ts
+++ backend/src/modules/Video/Video.controller.ts
@@ -1,4 +1,4 @@
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import { processAudio, UPLOAD_PATH } from "../../utils/runpy.js";
 import { deleteFile } from "../../utils/deletefiles.js";
 import { deleteFolder } from "../../utils/deletefolder.js";
@@ -6,69 +6,61 @@ import { readFile } from "fs/promises";
 import supabase from "../../config/supabase.js";
 import prisma from "../../config/prisma.js";
 import path from "path";
+import APIError from "../../utils/APIError.js";
+import { nextTick } from "process";
 
-export const createVideo = async (req: Request, res: Response) => {
-    try{
-        const audioPath = req.file?.path;
-        const audioName = req.file?.filename;
-        const n = audioName?.length!
-        const videoName = audioName?.substring(0, n-1) + "4";
-        await processAudio(audioPath!, videoName!);
+export const createVideo = async (req: Request, res: Response, next: NextFunction) => {
+    const audioPath = req.file?.path;
+    const audioName = req.file?.filename;
+    const n = audioName?.length!
+    const videoName = audioName?.substring(0, n-1) + "4";
+    await processAudio(audioPath!, videoName!);
 
-        const videoPath = path.join(UPLOAD_PATH, videoName);
+    const videoPath = path.join(UPLOAD_PATH, videoName);
 
-        if(audioPath){
-            await deleteFile(audioPath);
-        }
+    if(audioPath){
+        await deleteFile(audioPath);
+    }
 
-        // supabase
-        const storage = `videos/${req.User.id}/${videoName}`;
-        const videoBuffer = await readFile(videoPath);
-        const { error: uploadError } = await supabase.storage
-            .from("videos")
-            .upload(storage, videoBuffer, {
-                contentType: "video/mp4",
-                upsert: false
-            })
-
-        if(uploadError){
-            // upload error
-        }
-
-        const { data: urlData } = supabase.storage
-            .from("videos")
-            .getPublicUrl(storage);
-
-        
-        const video = await prisma.video.create({
-            data:{
-                title: videoName,
-                url: urlData.publicUrl,
-                storageKey: storage,
-                userId: req.User.id,
-            }
+    // supabase
+    const storage = `videos/${req.User.id}/${videoName}`;
+    const videoBuffer = await readFile(videoPath);
+    const { error: uploadError } = await supabase.storage
+        .from("videos")
+        .upload(storage, videoBuffer, {
+            contentType: "video/mp4",
+            upsert: false
         })
 
-        await deleteFolder(UPLOAD_PATH);
+    if(uploadError){
+        return next(new APIError("Internal server error", 500));
+    }
 
-        res.status(200).json({
-            status: "Success",
-            data: {
-                video: video
-            },
-        });
-    }
-    catch(error){
-        res.json(500).json({
-            status: "Faild",
-            error: {
-                error
-            },
-        })
-    }
+    const { data: urlData } = supabase.storage
+        .from("videos")
+        .getPublicUrl(storage);
+
+    
+    const video = await prisma.video.create({
+        data:{
+            title: videoName,
+            url: urlData.publicUrl,
+            storageKey: storage,
+            userId: req.User.id,
+        }
+    })
+
+    await deleteFolder(UPLOAD_PATH);
+
+    res.status(200).json({
+        status: "Success",
+        data: {
+            video: video
+        },
+    });
 }
 
-export const deleteVideo = async (req: Request, res: Response) => {
+export const deleteVideo = async (req: Request, res: Response, next: NextFunction) => {
     const id = req.params.id!;
 
     const video = await prisma.video.findUnique({
@@ -78,8 +70,7 @@ export const deleteVideo = async (req: Request, res: Response) => {
     });
 
     if(!video){
-        // error
-        throw new Error();
+        return next(new APIError("Video not found", 404));
     }
 
     const { error: deleteError } = await supabase.storage
@@ -87,7 +78,7 @@ export const deleteVideo = async (req: Request, res: Response) => {
         .remove([video.storageKey]);
 
     if(deleteError){
-        // handle error
+        return next(new APIError("Internal server error", 500));
     }
 
     await prisma.video.delete({
@@ -97,7 +88,7 @@ export const deleteVideo = async (req: Request, res: Response) => {
     })
 
     res.status(200).json({
-
+        status: "Success",
     })
 }
 
@@ -106,10 +97,12 @@ export const getAllVideos = async (req: Request, res: Response) => {
         where: {
             userId: req.User.id
         },
-    }
-    );
+    });
 
     res.status(200).json({
-        
+        status: "Success",
+        data: {
+            videos: videos
+        }
     })
 }
